0ebe5ba35aebf342d6a2f03d88d3f9d790ef1b04
Reid Roberts <reidrobertscs@gmail.com>
2023-06-26 20:00:23 -0500

    practice POST forms

---
+++ flask/POST Form Practice/server.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, request, redirect
+app = Flask(__name__)
+# our index route will handle rendering our form
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/users', methods=['POST'])
+def create_user():
+    print('Got POST info')
+    print(request.form)
+    return redirect('/')
+if __name__ == "__main__":
+    app.run(debug=True)
+
