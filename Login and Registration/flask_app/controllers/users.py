c5569d268d85ff00d853455c8cedfbe159c9f744
Reid Roberts <reidrobertscs@gmail.com>
2023-07-13 20:12:53 -0500

    created registration form

--- Login and Registration/flask_app/controllers/users.py
+++ Login and Registration/flask_app/controllers/users.py
@@ -1,2 +1,35 @@
 from flask_app.models.user import User
 from flask_app import app
+from flask import Flask,render_template,redirect,request, session
+from flask_bcrypt import Bcrypt
+bcrypt = Bcrypt(app)
+
+@app.route('/')
+def root():
+    return render_template('index.html')
+
+@app.route('/register', methods=["POST"])
+def register():
+    if not User.validate_user(request.form):
+        return redirect('/')
+
+    data = {
+        'first_name': request.form['first_name'],
+        'last_name': request.form['last_name'],
+        'email': request.form['email'],
+        'password': bcrypt.generate_password_hash(request.form['pw'],)
+    }
+    new_user_id = User.save(data)
+    session['user_id'] = new_user_id
+    return redirect('welcome')
+
+@app.route('/login', methods=["POST"])
+def login():
+
+
+    return redirect('/welcome')
+
+@app.route('/welcome')
+def welcome():
+
+    return render_template('welcome_page.html')
--- Login and Registration/flask_app/models/user.py
+++ Login and Registration/flask_app/models/user.py
@@ -1,1 +1,62 @@
 from flask_app.config.mysqlconnection import connectToMySQL
+import re
+from flask import flash
+
+class User:
+    DB = "login_and_registration_schema"
+    def __init__(self,data):
+        self.first_name = data['first_name']
+        self.last_name = data['last_name']
+        self.email = data['email']
+        self.password = data['password']
+        self.created_at = data['created_at']
+        self.updated_at = data['updated_at']
+
+    @classmethod
+    def get_user(cls, data):
+        query = '''SELECT * FROM users WHERE id = %(id)s'''
+        user = connectToMySQL(cls.DB).query_db(query, data)
+        return cls(user[0])
+
+    @classmethod
+    def save(cls, data):
+        query = '''INSERT INTO users (first_name, last_name, email, password)
+                    VALUES (%(first_name)s, %(last_name)s, %(email)s, %(password)s);'''
+        return connectToMySQL(cls.DB).query_db(query, data)
+
+            
+    @staticmethod
+    def validate_user(user):
+        is_valid = True
+        name_pattern = r'^[a-zA-Z]+$'
+        email_pattern = r'^[\w\.-]+@[\w\.-]+\.\w+$'
+
+        query = '''SELECT email FROM users WHERE email = %(email)s;'''
+        results = connectToMySQL(User.DB).query_db(query, user)
+
+        if len(user['first_name']) < 2 or not re.match(name_pattern, user['first_name']):
+            flash("First name must be longer than 2 characters and only consist of letters!", "register")
+            is_valid = False
+
+        if len(user['last_name']) < 2 or not re.match(name_pattern, user['last_name']):
+            flash("Last name must be longer than 2 characters and only consist of letters!", "register")
+            is_valid = False
+
+        # must get all emails from DB
+        if results:
+            flash("Email already being used!", "register")
+            is_valid = False
+
+        if not re.match(email_pattern, user['email']):
+            flash("Invalid email format!", "register")
+            is_valid = False
+
+        if len(user['pw']) < 8:
+            flash("Invalid password!", "register")
+            is_valid = False
+
+        if user['pw'] != user['confirm_pw']:
+            flash("Passwords do not match!", "register")
+            is_valid = False
+
+        return is_valid
